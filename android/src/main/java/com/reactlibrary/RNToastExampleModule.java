caef9e4c66106f6601c79a2e68ab48cbee41bd23
vudinhnghia <vudinhnghia.hut@gmail.com>
2019-02-21 10:49:53 +0700

    init

---
+++ android/src/main/java/com/reactlibrary/RNToastExampleModule.java
@@ -0,0 +1,41 @@
+
+package com.reactlibrary;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RNToastExampleModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+  private static final  String DURATION_SHORT_KEY = "SHORT";
+  private static final String DURATION_LONG_KEY = "LONG";
+
+  public RNToastExampleModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNToastExample";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+    return constants;
+  }
+
+  @ReactMethod
+  public void show(String message, int duration){
+    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+}
